a11c6817b0c16d68ed29d752642f480928adf671
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-03 00:00:00 +0000

    Improved removing/appending multiple item sets.

--- src/Job/AttachItemsToItemSets.php
+++ src/Job/AttachItemsToItemSets.php
@@ -12,6 +12,11 @@ class AttachItemsToItemSets extends AbstractJob
     protected $api;
 
     /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
      * @var \Laminas\Log\Logger
      */
     protected $logger;
@@ -32,14 +37,12 @@ class AttachItemsToItemSets extends AbstractJob
         $services = $this->getServiceLocator();
         $this->api = $services->get('Omeka\ApiManager');
         $this->logger = $services->get('Omeka\Logger');
+        $this->entityManager = $services->get('Omeka\EntityManager');
 
         // The reference id is the job id for now.
         $referenceIdProcessor = new \Laminas\Log\Processor\ReferenceId();
         $referenceIdProcessor->setReferenceId('dynis/attach_to_itemset/job_' . $this->job->getId());
         $this->logger->addProcessor($referenceIdProcessor);
-
-        /** @var \Doctrine\ORM\EntityManager $entityManager */
-        $entityManager = $services->get('Omeka\EntityManager');
 
         // Null means all dynamic item sets.
         $itemSetIds = $this->getArg('item_set_ids', null);
@@ -60,6 +63,23 @@ class AttachItemsToItemSets extends AbstractJob
             ['total' => count($itemSetIds)]
         );
 
+        // Process by step to avoid to update items multiple times.
+
+        // Clean the list of item sets.
+        $itemSetIds = array_unique(array_map('intval', $itemSetIds));
+        $itemSetIds = array_combine($itemSetIds, $itemSetIds);
+
+        // List existing item sets.
+        $existingItemSets = $this->api->search('item_sets', ['id' => $itemSetIds], ['returnScalar' => 'id'])->getContent();
+        if (count($existingItemSets) !== count($itemSetIds)) {
+            $this->logger->notice(
+                'The following item sets does not exist or you don’t have the right to manage them: #{item_set_ids}.', // @translate
+                ['item_set_ids' => implode(', #', array_diff_key($itemSetIds, $existingItemSets))]
+            );
+            $itemSetIds = array_intersect_key($itemSetIds, $existingItemSets);
+        }
+
+        // Clean queries. Normally useless.
         foreach ($itemSetIds as $itemSetId) {
             $query = $queries[$itemSetId] ?? null;
             if (is_string($query)) {
@@ -67,8 +87,43 @@ class AttachItemsToItemSets extends AbstractJob
                 parse_str($query, $nQuery);
                 $query = $nQuery ?: null;
             }
-            $this->attachItemsToItemSet($itemSetId, $query);
-            $entityManager->clear();
+            $queries[$itemSetId] = $query;
+        }
+
+        $queries = array_filter($queries);
+
+        // Skip item sets without query.
+        $itemSetsWithoutQuery = array_diff_key($itemSetIds, $queries);
+        if (count($itemSetsWithoutQuery)) {
+            $this->logger->info(
+                'The following item sets have no more query and items attached to it are kept: #{item_set_ids}.', // @translate
+                ['item_set_ids' => implode(', #', $itemSetsWithoutQuery)]
+            );
+        }
+
+        // Check remaining item sets.
+        $itemSetIds = array_intersect_key($itemSetIds, $queries);
+        if (!count($itemSetIds)) {
+            $this->logger->info(
+                'No more item sets to process.' // @translate
+            );
+            return;
+        }
+
+        // There are two strategies:
+        // - process by item set, so items may be managed multiple times;
+        // - process by item, removing and appending item sets one time.
+        // This second way seems quicker, but requires to prepare all items.
+        // But the batch update process use two loops to remove and to append
+        // item sets. And each item may not have the same item sets to remove
+        // and append. And in fact, batch update is a loop on update.
+        // So the two strategies are probably similar in real cases.
+
+        // Detach all item sets.
+        foreach ($itemSetIds as $itemSetId) {
+            $this->attachItemsToItemSet($itemSetId, $queries[$itemSetId]);
+            $this->entityManager->flush();
+            $this->entityManager->clear();
         }
 
         $this->logger->info(
@@ -77,28 +132,10 @@ class AttachItemsToItemSets extends AbstractJob
         );
     }
 
-    protected function attachItemsToItemSet(int $itemSetId, ?array $query): void
+    protected function attachItemsToItemSet(int $itemSetId, array $query): void
     {
-        try {
-            $this->api->read('item_sets', ['id' => $itemSetId]);
-        } catch (\Exception $e) {
-            $this->logger->notice(
-                'The item set #{item_set_id} does not exist or you don’t have the right to manage it.', // @translate
-                ['item_set_id' => $itemSetId]
-            );
-            return;
-        }
-
-        if (!$query) {
-            $this->logger->info(
-                'The item set #{item_set_id} has no more query and items attached to it are kept.', // @translate
-                ['item_set_id' => $itemSetId]
-            );
-            return;
-        }
-
         $existingItemIds = $this->api->search('items', ['item_set_id' => $itemSetId], ['returnScalar' => 'id'])->getContent();
-        $newItemIds = $query ? $this->api->search('items', $query, ['returnScalar' => 'id'])->getContent() : [];
+        $newItemIds = $this->api->search('items', $query, ['returnScalar' => 'id'])->getContent();
 
         /**
          * Batch update the resources in chunks to fix a memory issue.
@@ -115,10 +152,19 @@ class AttachItemsToItemSets extends AbstractJob
                     return;
                 }
                 $this->api->batchUpdate('items', $idsChunk, ['o:item_set' => [$itemSetId]], ['continueOnError' => true, 'collectionAction' => 'remove', 'isPartial' => true]);
-                $this->logger->info(
-                    '{count}/{total} items detached from item set #{item_set_id}.', // @translate
-                    ['count' => min(++$i * 100, count($detachItemIds)), 'total' => count($detachItemIds), 'item_set_id' => $itemSetId]
-                );
+                if (count($detachItemIds) <= 100) {
+                    $this->logger->info(
+                        '{total} items detached from item set #{item_set_id}.', // @translate
+                        ['total' => count($detachItemIds), 'item_set_id' => $itemSetId]
+                    );
+                } else {
+                    $this->logger->info(
+                        '{count}/{total} items detached from item set #{item_set_id}.', // @translate
+                        ['count' => min(++$i * 100, count($detachItemIds)), 'total' => count($detachItemIds), 'item_set_id' => $itemSetId]
+                    );
+                }
+                $this->entityManager->flush();
+                $this->entityManager->clear();
             }
         }
 
@@ -151,10 +197,19 @@ class AttachItemsToItemSets extends AbstractJob
                     $item['o:item_set'][] = ['o:id' => $itemSetId];
                     $this->api->update('items', $id, $item, ['continueOnError' => true, 'collectionAction' => 'append', 'isPartial' => true]);
                 }
-                $this->logger->info(
-                    '{count}/{total} new items attached to item set #{item_set_id}.', // @translate
-                    ['count' => min(++$i * 100, count($newItemIds)), 'total' => count($newItemIds), 'item_set_id' => $itemSetId]
-                );
+                if (count($newItemIds) <= 100) {
+                    $this->logger->info(
+                        '{total} new items attached to item set #{item_set_id}.', // @translate
+                        ['total' => count($newItemIds), 'item_set_id' => $itemSetId]
+                    );
+                } else {
+                    $this->logger->info(
+                        '{count}/{total} new items attached to item set #{item_set_id}.', // @translate
+                        ['count' => min(++$i * 100, count($newItemIds)), 'total' => count($newItemIds), 'item_set_id' => $itemSetId]
+                    );
+                }
+                $this->entityManager->flush();
+                $this->entityManager->clear();
             }
         }
 
